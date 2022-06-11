108fd19651b05e75255e01be5321e604211162db
sirishtrainingacct <107283785+sirishtrainingacct@users.noreply.github.com>
2022-06-11 10:16:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.header ('Breakfast Menu')
 streamlit.text ('😇Omega 3 & Blueberry Oatmeal')
 streamlit.text ('😜Kale,Spinach and Rocket Smoothie')
 streamlit.text ('😊Hard Boiled Free range Egg')
+
+streamlit.header ('😰 Build your own smoothie')
