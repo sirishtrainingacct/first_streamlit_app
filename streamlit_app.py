f37fdd3dec8a0952015bf90b7e2bcfe20a4b4fae
sirishtrainingacct <107283785+sirishtrainingacct@users.noreply.github.com>
2022-06-11 10:26:07 -0500

    Update streamlit_app.py
    added streamlit dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,4 @@ streamlit.text ('😊Hard Boiled Free range Egg')
 streamlit.header ('😰 Build your own smoothie')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamkit.dataframe('my_fruit_list)
